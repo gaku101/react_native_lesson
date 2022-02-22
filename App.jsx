63281459f8e9a05e08aacec33025655689653d28
takehiro <takehiro-r133@hotmail.co.jp>
2022-02-22 18:05:06 +0900

    creaet memoEditScreen and memoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,7 +1,9 @@
 import React from "react"
+import MemoCreateScreen from "./src/screens/MemoCreateScreen"
+// import MemoEditScreen from "./src/screens/MemoEditScreen"
 // import MemoDetailScreen from "./src/screens/MemoDetailScreen"
-import MemoListScreen from "./src/screens/MemoListScreen"
+// import MemoListScreen from "./src/screens/MemoListScreen"
 
 export default function App() {
-  return <MemoListScreen />
+  return <MemoCreateScreen />
 }
