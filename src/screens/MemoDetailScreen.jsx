260971649bc81a187cc994e7c6a5825aa53c1aa0
takehiro <takehiro-r133@hotmail.co.jp>
2022-02-25 19:58:08 +0900

    update memo data on MemoEditScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -46,7 +46,10 @@ export default function MemoDetailScreen(props) {
         style={{ top: 60, bottom: "auto" }}
         name='edit-2'
         onPress={() => {
-          navigation.navigate("MemoEdit")
+          navigation.navigate("MemoEdit", {
+            id: memo.id,
+            bodyText: memo.bodyText,
+          })
         }}
       />
     </View>
